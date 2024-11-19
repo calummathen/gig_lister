680eecb50fc9bacdb0144fda9dd15ed617bd45e4
Calum Mathen <calummathen@hotmail.co.uk>
2024-11-19 10:45:45 +0000

    Added props to Gig component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,22 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Gig/>
+      <Gig 
+      name="Foo Fighters"
+      image="https://cdn-p.smehost.net/sites/005297e5d91d4996984e966fac4389ea/wp-content/uploads/2017/06/00c04d57e42c56f9116eef3f1df14d53-800x800.jpg" 
+      alt = "Foo fighters black and white image"
+      description="Second stop of the UK Tour" 
+      date="21:00 21/12/2024" 
+      location="London 02 Academy"
+      />
+      <Gig 
+      name="Burna Boy"
+      image="https://www.bellanaijastyle.com/wp-content/uploads/2024/08/BurnaBoy.jpg" 
+      alt = "Burna boy sitting with sunglasses on"
+      description="Exclusive performance" 
+      date="19:00 28/12/2024" 
+      location="Brixton Academy"
+      />
     </>
   );
 }
--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,13 +1,15 @@
 import "../Gig.css";
 
-const Gig = () => {
+const Gig = ({name, image, alt, description, date, location}) => {
   return (
     <div>
-      <h3>Foo Fighters</h3>
-      <img className="photo" src="https://cdn-p.smehost.net/sites/005297e5d91d4996984e966fac4389ea/wp-content/uploads/2017/06/00c04d57e42c56f9116eef3f1df14d53-800x800.jpg" alt="Black and white picture of foo fighters"></img>
-      <p id="description" className="description">Second stop of the UK Tour</p>
-      <p id="event-time-and-date" className="date">21:00 21/12/2024</p>
-      <p id="event-location" className="location">London 02 Academy</p>
+      <h3>{name}</h3>
+      <div className="photo">
+        <img src={image} alt={alt} />
+      </div>
+      <p id="description" className="description">{description}</p>
+      <p id="event-time-and-date" className="date">{date}</p>
+      <p id="event-location" className="location">{location}</p>
     </div>
 
   )
